36be4d275ba8ce9b1b29bfc9c8ca697328bb4b7b
bhagyesh76 <82301604+bhagyesh76@users.noreply.github.com>
2024-08-20 20:05:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ st.write(
 
 from snowflake.snowpark.functions import col
 
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
